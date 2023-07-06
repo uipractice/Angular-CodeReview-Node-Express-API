0e8f5561536408bdf5ee6530c7318c081b216b8c
rgaddam <rgaddam@evoketechnologies.com>
2023-07-06 19:43:59 +0530

    mongodb account changes

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,8 @@
 const config = require("../config");
 const mongojs = require('mongojs');
-const db = mongojs(`mongodb+srv://${config.DB_USER}:${config.DB_PASSWORD}@cluster0.juatdby.mongodb.net/${config.DATABASE}`, ['details']);
+const db = mongojs(
+  `mongodb+srv://${config.DB_USER}:${config.DB_PASSWORD}@cluster0.4tsma3g.mongodb.net/${config.DATABASE}`,
+  ["details"]
+);
 
 module.exports = db;
