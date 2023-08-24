70eddd5453f799f364d30d405c81ce76414c1a1f
Ramarao Gaddam <rgaddam@evoketechnologies.com>
2023-08-24 21:27:43 +0530

    swagger implementation

--- swagger.js
+++ swagger.js
@@ -1,4 +1,4 @@
-const swaggerAutogen = require("swagger-autogen")();
+const swaggerAutogen = require("swagger-autogen")({ openapi: "3.0.0" });
 
 const outputFile = "./swagger_output.json";
 const endpointsFiles = ["./routing/index"];
